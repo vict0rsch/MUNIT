ebac2e895850e2924ed42bdee18e85c0b6c9b018
vict0rsch <vsch@protonmail.com>
2019-07-26 23:20:59 -0600

    for beluga

--- train_process.sh
+++ train_process.sh
@@ -4,9 +4,11 @@
 #SBATCH --mem=24G                        # Ask for 32 GB of RAM
 #SBATCH -o /network/tmp1/schmidtv/munit/disen_v1_shift_single/slurm-%j.out  # Write the log in $SCRATCH
 
-. /network/home/schmidtv/anaconda3/etc/profile.d/conda.sh
-conda activate munit
+#. /network/home/schmidtv/anaconda3/etc/profile.d/conda.sh
+#conda activate munit
 export COMET_API_KEY="5jmii3mKCc5WbSWZ6w9L6gzy0"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ai/modules/cuda-10.0/lib64/:/usr/local/cuda-9.0/lib64/
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ai/modules/cuda-10.0/lib64/:/usr/local/cuda-9.0/lib64/
+
+source ~/MUNIT/munitenv/bin/activate
 
 exec $@
